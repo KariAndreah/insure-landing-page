52cece134b5688b7e90ea01d6b1a957f8be08651
Kari Burt <kandreahburt@gmail.com>
2020-04-20 01:06:43 -0500

    working on responsive navigation

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function toggleNav() {
+const mainNav = document.getElementById("js-menu");
+const navBarToggle = document.getElementById("js-navbar-toggle");
 
-  }
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
+
